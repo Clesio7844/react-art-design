1ad3a48339ebeeb696249cb6694d68871b5f2a71
david <daviddecastro42@live.co.uk>
2021-03-09 02:28:30 +0000

    work on responsive menu using drawer component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Anchor } from 'antd';
+import { Anchor, Drawer, Button } from 'antd';
 
 const { Link } = Anchor;
 
 function AppHeader() {
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
   return (
     <div className='container-fluid'>
       <div className='header'>
@@ -13,15 +21,38 @@ function AppHeader() {
           <a href='https://ressum-portfolio.herokuapp.com/'>DLCC</a>
         </div>
 
-        <Anchor targetOffset='65'>
-          <Link href='#hero' title='Home' />
-          <Link href='#about' title='About' />
-          <Link href='#feature' title='Feature' />
-          <Link href='#works' title='How it Works' />
-          <Link href='#faq' title='Faq' />
-          <Link href='#pricing' title='pricing' />
-          <Link href='#contact' title='contact' />
-        </Anchor>
+        <div className='mobileHidden'>
+          <Anchor targetOffset='65'>
+            <Link href='#hero' title='Home' />
+            <Link href='#about' title='About' />
+            <Link href='#feature' title='Feature' />
+            <Link href='#works' title='How it Works' />
+            <Link href='#faq' title='Faq' />
+            <Link href='#pricing' title='pricing' />
+            <Link href='#contact' title='contact' />
+          </Anchor>
+        </div>
+        <div className='mobileVisible'>
+          <Button type='primary' onClick={showDrawer}>
+            <i className='fas fa-bars'></i>
+          </Button>
+          <Drawer
+            placement='right'
+            closable={false}
+            onClose={onClose}
+            visible={visible}
+          >
+            <Anchor targetOffset='65'>
+              <Link href='#hero' title='Home' />
+              <Link href='#about' title='About' />
+              <Link href='#feature' title='Feature' />
+              <Link href='#works' title='How it Works' />
+              <Link href='#faq' title='Faq' />
+              <Link href='#pricing' title='pricing' />
+              <Link href='#contact' title='contact' />
+            </Anchor>
+          </Drawer>
+        </div>
       </div>
     </div>
   );
